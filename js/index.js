1bc5dc4cba8a3f9e9267c452da70294c9f8804a3
camila yatabe <camila.yatabe@gmail.com>
2023-05-17 10:22:37 -0300

    first

---
+++ js/index.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const controls = document.querySelectorAll('.control');
+    let currentItem = 0;
+    const items = document.querySelectorAll('.item');
+    const maxItem = items.length;
+  
+    controls.forEach(control => {
+      control.addEventListener('click', () => {
+        const isLeft = control.classList.contains('arrow-left');
+  
+        if (isLeft) {
+          currentItem -= 3;
+        } else {
+          currentItem += 3;
+        }
+  
+        if (currentItem >= maxItem) {
+          currentItem = 0;
+        }
+  
+        if (currentItem < 0) {
+          currentItem = maxItem - 1;
+        }
+  
+        items.forEach(item => item.classList.remove('current-item'));
+
+        function flipPage() {
+            const book = document.querySelector('.item');
+            book.classList.toggle('flipped');
+          }
+          
+  
+        items[currentItem].scrollIntoView({
+          inline: "center",
+          behavior: 'smooth',
+          block: "nearest",
+        });
+  
+        items[currentItem].classList.add('current-item');
+      });
+    });
+  });
+  
