4b80d5324b4761d552beda22bc1924cbaad53a47
camiyuka <camila.yatabe@gmail.com>
2023-05-22 16:27:46 -0300

    JS: centralizar e aguardar ao clicar no livro

--- js/index.js
+++ js/index.js
@@ -1,43 +1,56 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const controls = document.querySelectorAll('.control');
-    let currentItem = 0;
-    const items = document.querySelectorAll('.item');
-    const maxItem = items.length;
-  
-    controls.forEach(control => {
-      control.addEventListener('click', () => {
-        const isLeft = control.classList.contains('arrow-left');
-  
-        if (isLeft) {
-          currentItem -= 2;
-        } else {
-          currentItem += 2;
-        }
-  
-        if (currentItem >= maxItem) {
-          currentItem = 0;
-        }
-  
-        if (currentItem < 0) {
-          currentItem = maxItem - 1;
-        }
-  
-        items.forEach(item => item.classList.remove('current-item'));
+  const controls = document.querySelectorAll('.control');
+  let currentItem = 0;
+  const items = document.querySelectorAll('.item');
+  const maxItem = items.length;
 
-        function flipPage() {
-            const book = document.querySelector('.item');
-            book.classList.toggle('flipped');
-          }
-          
-  
-        items[currentItem].scrollIntoView({
-          inline: "center",
-          behavior: 'smooth',
-          block: "nearest",
-        });
-  
-        items[currentItem].classList.add('current-item');
+  controls.forEach(control => {
+    control.addEventListener('click', () => {
+      const isLeft = control.classList.contains('arrow-left');
+
+      if (isLeft) {
+        currentItem -= 2;
+      } else {
+        currentItem += 2;
+      }
+
+      if (currentItem >= maxItem) {
+        currentItem = 0;
+      }
+
+      if (currentItem < 0) {
+        currentItem = maxItem - 1;
+      }
+
+      items.forEach(item => item.classList.remove('current-item'));
+      items[currentItem].scrollIntoView({
+        inline: "center",
+        behavior: 'smooth',
+        block: "nearest",
       });
+
+      items[currentItem].classList.add('current-item');
     });
   });
+
+  function bookCenter(event) {
+    var item = event.currentTarget; // Usar currentTarget em vez de target
+    var anchor = item.closest('a'); // Encontra o elemento âncora mais próximo
+
+    item.classList.add('center');
+    item.style.opacity = '1'; // Ajusta a opacidade para um valor desejado
+    item.scrollIntoView='smooth';
   
+      
+    setTimeout(function() {
+      window.location.href = anchor.href;
+    }, 1000); // Tempo em milissegundos antes do redirecionamento (aqui definido como 1 segundo)
+    event.preventDefault();
+  }
+
+  items.forEach(item => {
+    item.addEventListener('click', bookCenter);
+
+  });
+
+});
